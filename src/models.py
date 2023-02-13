592ed55e83916f7ef54e2438de2d255c478bfa47
pyslarash <alex.pyslarash@gmail.com>
2023-02-13 00:37:11 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), unique=True, nullable=False)
+    password = Column(String(250), nullable=False)
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250), unique=True, nullable=False)
+    favorites = relationship("Favorites", back_populates="user")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorites(Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), ForeignKey('user.username'))
+    planets = Column(String(250), ForeignKey('planets.planet_name'))
+    characters = Column(String(250), ForeignKey('characters.character_name'))
+    user = relationship("User", back_populates="favorites")
+    planet = relationship("Planets", back_populates="favorites")
+    character = relationship("Characters", back_populates="favorites")
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250), nullable=False)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String(250))
+    terrain = Column(String(250))
+    favorites = relationship("Favorites", back_populates="planet")
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(250), nullable=False)
+    height = Column(Integer)
+    weight = Column(Integer)
+    birth_year = Column(String(5))
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
+    hail_color = Column(String(250))
+    favorites = relationship("Favorites", back_populates="character")
+   
 
     def to_dict(self):
         return {}
